70a9c7e2e0c481fbf84c17a396f71ae391d1f514
Ishrat2413 <ishratrintu2413@gmail.com>
2025-11-19 14:41:47 +0600

    project struct ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ishrat Jahan",
+    author_email="ishratjahanrintu72@gmail.com",
+    packages=find_packages()
+)
